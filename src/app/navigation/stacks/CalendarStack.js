3829f36e0e001ffe32f546a009bb7018cfa60ddf
hamsu12 <hamsj2413@smail.kongju.ac.kr>
2025-12-17 03:25:51 +0900

    chore: rename calender files

--- src/app/navigation/stacks/CalendarStack.js
+++ src/app/navigation/stacks/CalendarStack.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { View, Text, Button } from 'react-native';
-import CalendarScreen from "../../../features/calendar/screens/CalerdarScreen";
+import CalendarScreen from "../../../features/calendar/screens/CalendarScreen";
 
 const Stack = createNativeStackNavigator();
 
