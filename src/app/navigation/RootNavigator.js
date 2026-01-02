bbc8bfc8f4c55c368b8786fadd8f4c50641eb28d
hamsu12 <hamsj3808@gmail.com>
2026-01-02 22:35:51 +0900

    feat: 로딩 에러 화면 전체 오버레이 통일

--- src/app/navigation/RootNavigator.js
+++ src/app/navigation/RootNavigator.js
@@ -3,11 +3,13 @@ import React from "react";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import AuthStack from "./stacks/AuthStack";
 import MainTabs from "./MainTabs";
+import AppErrorBoundary from "../../shared/components/AppErrorBoundary";
 
 const Stack = createNativeStackNavigator();
 
 export default function RootNavigator() {
   return (
+      <AppErrorBoundary>
     <Stack.Navigator
       screenOptions={{headerShown: false}}  
       initialRouteName="Main"
@@ -15,5 +17,6 @@ export default function RootNavigator() {
       <Stack.Screen name="Auth" component={AuthStack} screenOptions={{headerShown: false}}/>
       <Stack.Screen name="Main" component={MainTabs} screenOptions={{headerShown: false}}/>
     </Stack.Navigator>
+      </AppErrorBoundary>
   );
 }
