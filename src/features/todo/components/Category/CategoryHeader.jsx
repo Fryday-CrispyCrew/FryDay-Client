a9449d9c2565b56e691cbfe2d89bd4a58a861919
bmbx08 <bmbx08@bu.ac.kr>
2026-01-28 05:17:19 +0900

    style: 카테고리 '+' 아이콘 크기 조정

--- src/features/todo/components/Category/CategoryHeader.jsx
+++ src/features/todo/components/Category/CategoryHeader.jsx
@@ -51,7 +51,7 @@ export default function CategoryHeader({
             hitSlop={10}
             style={styles.plusBtn}
           >
-            <PlusIcon width={24} height={24} color={colors.gr900} />
+            <PlusIcon width={18} height={18} color={colors.gr900} />
           </TouchableOpacity>
         ) : (
           // ✅ 우측 레이아웃 흔들림 방지용 자리 유지
