36db086d1e9ec6047d8641e04bafd907587747c9
hamsu12 <hamsj3808@gmail.com>
2026-01-28 04:23:19 +0900

    chore: ios 전역 폰트 스케일 차단

--- src/app/App.js
+++ src/app/App.js
@@ -1,20 +1,27 @@
-// src/app/App.js
-import React, {useEffect} from "react";
+import React from "react";
 import * as SplashScreen from "expo-splash-screen";
 import "react-native-gesture-handler";
+import { Text, TextInput, Platform } from "react-native";
 import "../../global.css";
 import AppProviders from "./AppProviders";
 import RootNavigator from "./navigation/RootNavigator";
 
+// iOS에서만 전역 폰트 스케일 차단
+if (Platform.OS === "ios") {
+    if (Text.defaultProps == null) Text.defaultProps = {};
+    Text.defaultProps.allowFontScaling = false;
+
+    if (TextInput.defaultProps == null) TextInput.defaultProps = {};
+    TextInput.defaultProps.allowFontScaling = false;
+}
+
 // 스플래시가 자동으로 사라지지 않게 설정
-SplashScreen.preventAutoHideAsync().catch(() => {
-  // 이미 호출된 경우 등의 에러는 무시해도 됨
-});
+SplashScreen.preventAutoHideAsync().catch(() => {});
 
 export default function App() {
-  return (
-    <AppProviders>
-      <RootNavigator />
-    </AppProviders>
-  );
+    return (
+        <AppProviders>
+            <RootNavigator />
+        </AppProviders>
+    );
 }
