20c0a74e5c5fa2e99702db39fbb76753b79c8c62
hamsu12 <hamsj3808@gmail.com>
2026-01-29 07:27:46 +0900

    chore: 전역 폰트 스케일 차단

--- src/app/App.js
+++ src/app/App.js
@@ -6,14 +6,12 @@ import "../../global.css";
 import AppProviders from "./AppProviders";
 import RootNavigator from "./navigation/RootNavigator";
 
-// iOS에서만 전역 폰트 스케일 차단
-if (Platform.OS === "ios") {
-    if (Text.defaultProps == null) Text.defaultProps = {};
-    Text.defaultProps.allowFontScaling = false;
+// 전 플랫폼 전역 폰트 스케일 차단
+if (Text.defaultProps == null) Text.defaultProps = {};
+Text.defaultProps.allowFontScaling = false;
 
-    if (TextInput.defaultProps == null) TextInput.defaultProps = {};
-    TextInput.defaultProps.allowFontScaling = false;
-}
+if (TextInput.defaultProps == null) TextInput.defaultProps = {};
+TextInput.defaultProps.allowFontScaling = false;
 
 // 스플래시가 자동으로 사라지지 않게 설정
 SplashScreen.preventAutoHideAsync().catch(() => {});
