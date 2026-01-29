e544491f3f8116927777fedbef7f2e28e53a54e0
bmbx08 <bmbx08@bu.ac.kr>
2026-01-29 15:12:41 +0900

    fix: 세션 유지 오류로 해당 코드 주석 처리

--- src/app/navigation/RootNavigator.js
+++ src/app/navigation/RootNavigator.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { getAccessToken } from "../../shared/lib/storage/tokenStorage";
+import React, {useEffect, useState} from "react";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {getAccessToken} from "../../shared/lib/storage/tokenStorage";
 
 import AuthStack from "./stacks/AuthStack";
 import OnboardingStack from "./stacks/OnboardingStack";
@@ -12,40 +12,41 @@ import CategoryStack from "./stacks/CategoryStack";
 const Stack = createNativeStackNavigator();
 
 export default function RootNavigator() {
-    const [ready, setReady] = useState(false);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  //   const [ready, setReady] = useState(false);
+  //   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    useEffect(() => {
-        (async () => {
-            const token = await getAccessToken();
-            setIsLoggedIn(!!token);
-            setReady(true);
-        })();
-    }, []);
+  //   useEffect(() => {
+  //     (async () => {
+  //       const token = await getAccessToken();
+  //       setIsLoggedIn(!!token);
+  //       setReady(true);
+  //     })();
+  //   }, []);
 
-    if (!ready) return null;
+  //   if (!ready) return null;
 
-    return (
-        <Stack.Navigator
-            id="root"
-            screenOptions={{ headerShown: false }}
-            initialRouteName={isLoggedIn ? "Main" : "Auth"}
-        >
-            <Stack.Screen name="Auth" component={AuthStack} />
-            <Stack.Screen name="Naming" component={NamingStack} />
-            <Stack.Screen name="Onboarding" component={OnboardingStack} />
-            <Stack.Screen name="Category" component={CategoryStack} />
-            <Stack.Screen name="Main" component={MainTabs} />
+  return (
+    <Stack.Navigator
+      id="root"
+      screenOptions={{headerShown: false}}
+      //   initialRouteName={isLoggedIn ? "Main" : "Auth"}
+      initialRouteName="Auth"
+    >
+      <Stack.Screen name="Auth" component={AuthStack} />
+      <Stack.Screen name="Naming" component={NamingStack} />
+      <Stack.Screen name="Onboarding" component={OnboardingStack} />
+      <Stack.Screen name="Category" component={CategoryStack} />
+      <Stack.Screen name="Main" component={MainTabs} />
 
-            <Stack.Group
-                screenOptions={{
-                    presentation: "transparentModal",
-                    contentStyle: { backgroundColor: "transparent" },
-                    animation: "fade",
-                }}
-            >
-                <Stack.Screen name="Marketing" component={MarketingModal} />
-            </Stack.Group>
-        </Stack.Navigator>
-    );
+      <Stack.Group
+        screenOptions={{
+          presentation: "transparentModal",
+          contentStyle: {backgroundColor: "transparent"},
+          animation: "fade",
+        }}
+      >
+        <Stack.Screen name="Marketing" component={MarketingModal} />
+      </Stack.Group>
+    </Stack.Navigator>
+  );
 }
