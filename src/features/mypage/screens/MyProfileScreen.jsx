4a48906e13ba3e7e569674945a05cd9de7ad35dc
hammmmmmsu <hamsj3808@gmail.com>
2026-04-08 01:45:43 +0900

    chore: 버전 정보 수정

--- src/features/mypage/screens/MyProfileScreen.jsx
+++ src/features/mypage/screens/MyProfileScreen.jsx
@@ -7,37 +7,37 @@ import MyPageMenu from "../components/MypageMenu";
 import Banner from "../../../shared/components/Banner";
 
 export default function MyProfileScreen() {
-    return (
-        <SafeAreaView className="flex-1 bg-gr" edges={["top", "bottom"]}>
-            <MyPageHeader showBackButton={false} title="설정" />
+  return (
+    <SafeAreaView className="flex-1 bg-gr" edges={["top", "bottom"]}>
+      <MyPageHeader showBackButton={false} title="설정" />
 
-            <ScrollView
-                showsVerticalScrollIndicator={false}
-                contentContainerStyle={{
-                    flexGrow: 1,
-                    paddingHorizontal: 20,
-                    paddingTop: 16,
-                    paddingBottom: 24,
-                    rowGap: 16,
-                }}
-            >
-                <MyPageMenu menuTitle="공지 사항" to="Notice" />
-                <MyPageMenu menuTitle="계정 설정" to="EditProfile" />
-                <MyPageMenu menuTitle="알림 설정" to="Alarm" />
-                <MyPageMenu menuTitle="이용 약관" to="Use" />
-                <MyPageMenu menuTitle="자주 묻는 질문" to="Qna" />
-                <MyPageMenu menuTitle="버전 정보" rightText="1.1ver" hideArrow />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          flexGrow: 1,
+          paddingHorizontal: 20,
+          paddingTop: 16,
+          paddingBottom: 24,
+          rowGap: 16,
+        }}
+      >
+        <MyPageMenu menuTitle="공지 사항" to="Notice" />
+        <MyPageMenu menuTitle="계정 설정" to="EditProfile" />
+        <MyPageMenu menuTitle="알림 설정" to="Alarm" />
+        <MyPageMenu menuTitle="이용 약관" to="Use" />
+        <MyPageMenu menuTitle="자주 묻는 질문" to="Qna" />
+        <MyPageMenu menuTitle="버전 정보" rightText="1.2ver" hideArrow />
 
-                {/* 배너 광고 */}
-                <View
-                    style={{
-                        alignSelf: "center",
-                        marginTop: 40,
-                    }}
-                >
-                    <Banner />
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    );
+        {/* 배너 광고 */}
+        <View
+          style={{
+            alignSelf: "center",
+            marginTop: 40,
+          }}
+        >
+          <Banner />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
