fbd909d2ecbec36fdb54df730f710192cf318824
hammmmmmsu <hamsj3808@gmail.com>
2026-04-28 16:14:35 +0900

    feat: borderbutton 컴포넌트 생성

---
+++ src/shared/components/BorderButton.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { TouchableOpacity, View } from "react-native";
+import AppText from "./AppText";
+import colors from "../styles/colors";
+
+export default function BorderButton({
+                                       text,
+                                       icon,
+                                       borderColor = colors.or,
+                                       textColor = colors.or,
+                                       onPress,
+                                     }) {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={onPress}
+      style={{
+        borderRadius: 24,
+        borderWidth: 1,
+        borderColor,
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        alignSelf: "flex-start",
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <AppText
+          variant="M600"
+          style={{
+            color: textColor,
+          }}
+        >
+          {text}
+        </AppText>
+
+        {icon && <View style={{ marginLeft: 6 }}>{icon}</View>}
+      </View>
+    </TouchableOpacity>
+  );
+}
