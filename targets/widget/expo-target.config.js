2b0fba050cc2f32a6eb6f2188411e02427ce9032
hammmmmmsu <hamsj3808@gmail.com>
2026-06-30 00:03:44 +0900

    chore: widget bundleID 수정

--- targets/widget/expo-target.config.js
+++ targets/widget/expo-target.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   type: "widget",
   name: "FrydayWidget",
-  bundleIdentifier: "com.cripycrew.fryday.widget",
+  bundleIdentifier: "com.crispycrew.fryday.widget",
   deploymentTarget: "17.0",
   entitlements: {
     "com.apple.security.application-groups": [
